282be7e56f2ca9000980766ac607380d664fac3b
Peter Boni <peter.boni@gmail.com>
2013-02-17 14:04:37 +0800

    Site updated at 2013-02-17 06:04:37 UTC

---
+++ media/fancybox/activate.js
@@ -0,0 +1,19 @@
+(function($){
+
+  $('.entry-content').each(function(i){
+      var _i = i;
+      $(this).find('img.fancybox').each(function(){
+          var img = $(this);
+          var title = img.attr("title");
+          var classes = img.attr("class");
+          img.removeClass("fancybox");
+          img.wrap('<a href="'+this.src+'" class="fancybox" rel="gallery'+_i+'" />');
+          if (title != "")
+          {
+              img.parent().attr("title", title);
+          }
+      });
+  });
+  $('.fancybox').fancybox();
+
+})(jQuery);
