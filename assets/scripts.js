0597430564b6889db8d26ca6b61702ccf1e444e8
Peter Boni <peter.boni@icloud.com>
2019-06-16 17:50:21 +0800

    Upgraded theme.

---
+++ assets/scripts.js
@@ -0,0 +1,8 @@
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+// make all images responsive - https://getbootstrap.com/docs/4.0/content/images/
+$(function() {
+	$("img").addClass("img-fluid");
+});
